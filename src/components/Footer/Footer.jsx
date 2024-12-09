db1fb33e764f648ac3d6cddda96942ba01f104f1
TREE-DIGITAL-MARKETING <treedigitalmarketing2024@gmail.com>
2024-12-09 15:45:39 +0530

    minor update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+import { motion } from "framer-motion";
+
+const Footer = () => {
+  return (
+    <footer> {/* Use <footer> tag instead of <Footer> */}
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 md:gap-4">
+
+          {/* first section */}
+          <div>
+            <h1>
+              the coding journey
+            </h1>
+            <p>
+              A complete log of this run can be found in: C:\\Users\\wi
+              nin\\AppData\\Local\\npm-cache\\_logs\\2024-12-09T08_30_42_092Z-debug-0.log
+            </p>
+          </div>
+
+          {/* second section */}
+          {/* third section */}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
